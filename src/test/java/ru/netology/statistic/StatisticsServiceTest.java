4f10df2069c3a87e2b329604dd567fc567b28511
Alinailka <alina-il@mail.ru>
2022-03-14 10:57:05 +0700

    Test 100%. Remake

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -13,4 +13,13 @@ public class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMoreMax() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 13};
+        long expected = 13;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
 }
